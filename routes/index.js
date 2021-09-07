cd50b6d12cc812f989ab834b55b68c2e2c2e8069
xuehua <2450588072@qq.com>
2021-09-07 18:31:53 +0800

    no message

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+const router = require('koa-router')()
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+let path = require('path')
+
+router.get('/htmlToImg', async (ctx, next) => {
+  const query = ctx.query
+  console.log('-query-',query)
+  const imgfrom = `http://${ctx.request.header.host}/static/html/jd.html`
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(imgfrom,{
+    timeout: 30 * 1000,
+    waitUntil: [
+      'load', //等待 “load” 事件触发
+    ]
+  });
+ const imgdata = await page.screenshot({
+    path: 'example.png', 
+    clip: {
+      x: 0,
+      y: 0,
+      width: 740,
+      height: 856,
+    },
+    encoding: 'base64'
+  });
+  const obj = {
+    code: 200,
+    data: imgdata
+  }
+  ctx.body = obj;
+
+  await page.close();
+  await browser.close();
+  // await next();
+})
+
+router.get('/testget', async (ctx, next) => {
+  ctx.body = {
+    code: 200,
+    msg: "hello im from get"
+  }
+})
+
+router.post('/testpost', async (ctx, next) => {
+  ctx.body = {
+    code: 200,
+    msg: "hello im from post"
+  }
+})
+
+
+
+module.exports = router
